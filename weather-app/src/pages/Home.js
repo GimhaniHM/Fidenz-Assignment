f348f703ac49df85d278f193c8a12dc3cd53baa0
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-07-31 22:49:01 +0530

    Home page is updated

--- weather-app/src/pages/Home.js
+++ weather-app/src/pages/Home.js
@@ -1,60 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useQuery } from 'react-query';
 import Card from '../components/Card';
-import axios from 'axios';
+//import axios from 'axios';
 import List from './../cities.json';
+import { CARD_IMAGES, CelciusValue } from '../shared/constants.js';
+import { getWeatherDataUrl } from '../shared/APIHelper.js';
 
 const Home = () => {
-  const [weatherData, setWeatherData] = useState([]);
-
-  useEffect(() => {
-    // Function to fetch weather data
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `http://api.openweathermap.org/data/2.5/group?id=${getCityIds()}&appid=1891735cfb394923413c67a90e76954f`
-        );
-        setWeatherData(response.data.list);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      }
-    };
-
-    fetchData();
-
-     // Set interval to fetch data every 5 minutes
-    const interval = setInterval(fetchData, 5 * 60 * 1000);
-    return () => {
-        clearInterval(interval);
-      };
-  }, []);
+  //const [weatherData, setWeatherData] = useState([]);
+  //const [loading, setLoading] = useState(true);
 
   const getCityIds = () => {
     return List.List.map((city) => city.CityCode).join(',');
+    //return List.List.map((city) => city.CityCode);
   };
+
+  console.log(getCityIds());
+
+  const { data, isLoading, error, isError } = useQuery("data", () => {
+    return fetch(
+      `http://api.openweathermap.org/data/2.5/group?id=${getCityIds()}&appid=1891735cfb394923413c67a90e76954f`
+    )
+      .then((response) => response.json());
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>Error! {error.message}</div>
+  }
+
+  /*if (!data || !data.list || data.list.length === 0) {
+    return <div>No data available</div>;
+  }*/
+
+  console.log(data);
 
   return (
     <div className="home_container">
-      <div className="home_card_container">
-        {weatherData.map((data, idx) => (
-          <Card
-            key={idx}
-            cityName={List.List[idx].CityName}
-            img={getCardImage(idx)}
-            status={data.weather[0].description}
-            temp={convertKelvinToCelsius(data.main.temp)}
-            pressure={data.main.pressure}
-            humidity={data.main.humidity}
-            visibility={data.visibility / 1000}
-            sunrise={data.sys.sunrise}
-            sunset={data.sys.sunset}
-            windSpeed={data.wind.speed}
-            windDegree={data.wind.deg}
-            tempMin={data.main.temp_min}
-            tempMax={data.main.temp_max}
-            time={new Date().toLocaleTimeString()}
-          />
-        ))}
-      </div>
+        <div className="home_card_container">
+          {data.list.map((data, idx) => (
+            <Card
+              key={idx}
+              cityName={List.List[idx].CityName}
+              img={CARD_IMAGES(idx)}
+              status={data.weather[0].description}
+              temp={CelciusValue(data.main.temp)}
+              pressure={data.main.pressure}
+              humidity={data.main.humidity}
+              visibility={data.visibility / 1000}
+              sunrise={data.sys.sunrise}
+              sunset={data.sys.sunset}
+              windSpeed={data.wind.speed}
+              windDegree={data.wind.deg}
+              tempMin={data.main.temp_min}
+              tempMax={data.main.temp_max}
+              time={new Date().toLocaleTimeString()}
+            />
+          ))}
+        </div>
     </div>
   );
 };
